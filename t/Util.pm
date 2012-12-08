9a9809c55c26db895753a5a5e45d7f510c683194
tokuhirom <tokuhirom@gmail.com>
2012-12-09 00:00:51 +0900

    added test case

---
+++ t/Util.pm
@@ -0,0 +1,20 @@
+package t::Util;
+use strict;
+use warnings;
+use utf8;
+use parent qw/Exporter/;
+use Karas;
+
+our @EXPORT = qw/create_karas/;
+
+sub create_karas {
+    require Test::Requires;
+    Test::Requires->import('DBD::SQLite');
+    my $db = Karas->new(
+        connect_info => ['dbi:SQLite::memory:']
+    );
+    return $db;
+}
+
+1;
+
